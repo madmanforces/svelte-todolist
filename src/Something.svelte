63d7b26d3b3104f6affaabc9ba7802dd2ba8d022
madmanforces <hwangcc1019@naver.com>
2022-01-11 23:15:53 +0900

    mount destroy before/after update

--- src/Something.svelte
+++ src/Something.svelte
@@ -1,9 +1,28 @@
 <script>
-    import {onMount} from 'svelte'
+    import {onMount, onDestroy, beforeUpdate, afterUpdate} from 'svelte'
 
-    onMount( () => {
-        console.log('Mounted')
+    let name = 'Something..'
+    let h1
+    
+
+    function moreDot() {
+        name += '.'
+    }
+    beforeUpdate(() => {
+        console.log('비포 업데이트!')
+        console.log(h1 && h1.innerText)
+    })
+    afterUpdate(() => {
+        console.log('에프터 업데이트!')
+        console.log(h1.innerText)
+    })
+    onMount(() => {
+        console.log('마운트됐어!')
+        h1 = document.querySelector('h1')
+        })
+    onDestroy(() => {
+        console.log('파괴 했다구!')
     })
 </script>
 
-<h1>Something</h1>
+<h1 on:click={moreDot}>{name}</h1>
